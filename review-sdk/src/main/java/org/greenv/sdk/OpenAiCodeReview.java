8281d95dde24ea2e4bf08c47039742255ff88d7f
@qq_52791268 <zzzzy66667@163.com>
2024-10-29 16:52:28 +0800

    fix: add main

--- review-sdk/src/main/java/org/greenv/sdk/OpenAiCodeReview.java
+++ review-sdk/src/main/java/org/greenv/sdk/OpenAiCodeReview.java
@@ -5,4 +5,7 @@ package org.greenv.sdk;
  * @since : 2024-10-29 15:03
  */
 public class OpenAiCodeReview {
+    public static void main(String[] args) {
+        System.out.println("测试执行");
+    }
 }
