6a1d05479a30846152d5fe3448dafc150cfe4f24
green <zzzzy66667@163.com>
2025-10-06 17:07:05 +0800

    feature:add yml

--- review-sdk/src/main/java/org/greenv/sdk/OpenAiCodeReview.java
+++ review-sdk/src/main/java/org/greenv/sdk/OpenAiCodeReview.java
@@ -1,11 +1,36 @@
 package org.greenv.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
 /**
  * @author : zhouyufei
  * @since : 2024-10-29 15:03
  */
 public class OpenAiCodeReview {
-    public static void main(String[] args) {
-        System.out.println("测试CodeReview");
+    public static void main(String[] args) throws Exception {
+        System.out.println("测试执行");
+
+        // 1.代码检出
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+
+        StringBuilder diffCode = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            diffCode.append(line);
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("Exited with code:" + exitCode);
+
+        System.out.println("评审代码：" + diffCode.toString());
+
     }
+
 }
